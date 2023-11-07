d7f00a2d1d5ae37454e41613d832500e7bc6c8bd
Aarush-Kapoor <kapoora1@wit.edu>
2023-11-07 14:37:31 -0500

    Updates Server and Client
    Client can send messages to a server GUI where it gets saved.

--- Shared-Document-FInal-Project/client.py
+++ Shared-Document-FInal-Project/client.py
@@ -2,29 +2,37 @@
 """
 Created on Thu Oct 26 16:28:01 2023
 
-@author: aarus
+@authors: Aarush Kapoor, Jenny Li Wang, Ayman Ali
+Client
 """
 import tkinter as tk
 import socket
-#while True:
+
+def send_message():
+    message = message_entry.get()
+    s.sendall(message.encode())
+    message_listbox.insert("end", f"Client: {message}")
+    if message == 'end':
+        s.close()
+        message_listbox.insert("end", "Connection closed")
+    message_entry.delete(0, "end")  # Clear the input field
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Port = 1234
 Host = socket.gethostname()
-    
+
 s.connect((Host, Port))
 
-while True:
-    
-    # Sending message to server
-    message = input('message: ')
-    s.sendall(message.encode())
-    if message == 'end':
-        break
-        s.close()
-    
-    # Recieving message from server
-    recievedMessage = s.recv(2048).decode()
-    print('server:', recievedMessage)
-    if recievedMessage == 'end':
-        break
-        s.close()
+root = tk.Tk()
+root.title("Client")
+
+message_listbox = tk.Listbox(root, bg="white")
+message_listbox.pack()
+
+message_entry = tk.Entry(root)
+message_entry.pack()
+
+send_button = tk.Button(root, text="Send", command=send_message)
+send_button.pack()
+
+root.mainloop()
