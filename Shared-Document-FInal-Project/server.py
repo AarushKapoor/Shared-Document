f8b62a88e015621997a544021b99d85a6f6fa46a
liwangjATWIT <114754652+liwangjATWIT@users.noreply.github.com>
2023-11-20 17:41:28 -0500

    added comments on server code

--- Shared-Document-FInal-Project/server.py
+++ Shared-Document-FInal-Project/server.py
@@ -11,20 +11,28 @@ import threading
 
 shared_document = ""  # Shared document content
 
+# Function that handles communication with the client
 def handle_client(client_socket):
     global shared_document
     while True:
         request = client_socket.recv(1024).decode()
+        #If client request to pull the document
         if request == "pull":
+            # Send the share document to the client
             client_socket.send(shared_document.encode())
+        # If the client request to save the document
         elif request.startswith("save:"):
             data = request.split(":", 1)[1]
             shared_document = data
+            # Message to the client that document has been saved
             client_socket.send(b"Saved")
+        # If client request to exit the commention    
         elif request == "exit":
+            # Close the client socket
             client_socket.close()
             break
 
+# Set up the server socket and begin listening to the client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Port = 1234
 s.bind(('', Port))
@@ -33,8 +41,10 @@ s.listen(20)
 print("Server is listening for clients...")
 
 while True:
+    # Accept client connection
     client_socket, addr = s.accept()
     print(f"Accepted connection from {addr}")
+    # A thread is created that handles client's requests
     client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-    client_handler.start()
+    client_handler.start() #Thread starts handling the client
 
