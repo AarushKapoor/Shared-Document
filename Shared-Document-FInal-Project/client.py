52c07a307d02524d392100c5f6b892eedecb874d
Aarush-Kapoor <kapoora1@wit.edu>
2023-11-19 19:14:51 -0500

    Update client.py

--- Shared-Document-FInal-Project/client.py
+++ Shared-Document-FInal-Project/client.py
@@ -30,6 +30,44 @@ def download_document():
         file.write(shared_document)
     print(f"Document downloaded as {file_path}")
 
+def make_text_bold():
+    current_tags = text.tag_names("sel.first")
+    if "bold" in current_tags:
+        text.tag_remove("bold", "sel.first", "sel.last")
+    else:
+        text.tag_add("bold", "sel.first", "sel.last")
+        text.tag_configure("bold", font=(font_family, font_size, "bold"))
+
+def make_text_italic():
+    current_tags = text.tag_names("sel.first")
+    if "italic" in current_tags:
+        text.tag_remove("italic", "sel.first", "sel.last")
+    else:
+        text.tag_add("italic", "sel.first", "sel.last")
+        text.tag_configure("italic", font=(font_family, font_size, "italic"))
+
+def make_text_underlined():
+    current_tags = text.tag_names("sel.first")
+    if "underline" in current_tags:
+        text.tag_remove("underline", "sel.first", "sel.last")
+    else:
+        text.tag_add("underline", "sel.first", "sel.last")
+        text.tag_configure("underline", underline=True)
+
+def change_font_size(event=None):
+    global font_size
+    try:
+        new_size = float(font_size_var.get())
+        if new_size > 0:
+            font_size = new_size
+            text.configure(font=(font_family, font_size))
+    except ValueError:
+        pass  # Ignore if the user enters a non-numeric value
+
+# Set default font family and font size
+font_family = "Helvetica"
+font_size = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Port = 1234
 Host = "10.220.45.216"
@@ -38,18 +76,44 @@ s.connect((Host, Port))
 root = tk.Tk()
 root.title("Shared Document Client")
 
-text = tk.Text(root, wrap="word", width=40, height=15)
+# Frame to organize the buttons at the top
+button_frame = tk.Frame(root)
+button_frame.pack(side="top", fill="x")
+
+# Buttons for actions
+pull_button = tk.Button(button_frame, text="Pull", command=pull_document)
+pull_button.pack(side="left")
+
+save_button = tk.Button(button_frame, text="Save", command=save_document)
+save_button.pack(side="left")
+
+download_button = tk.Button(button_frame, text="Download", command=download_document)
+download_button.pack(side="left")
+
+bold_button = tk.Button(button_frame, text="Bold", command=make_text_bold)
+bold_button.pack(side="left")
+
+italic_button = tk.Button(button_frame, text="Italic", command=make_text_italic)
+italic_button.pack(side="left")
+
+underline_button = tk.Button(button_frame, text="Underline", command=make_text_underlined)
+underline_button.pack(side="left")
+
+# Font size entry
+font_size_label = tk.Label(button_frame, text="Font Size:")
+font_size_label.pack(side="left")
+
+font_size_var = tk.StringVar()
+font_size_var.set(str(font_size))
+font_size_entry = tk.Entry(button_frame, textvariable=font_size_var)
+font_size_entry.pack(side="left")
+font_size_entry.bind("<Return>", change_font_size)
+
+# Text widget for document content
+text = tk.Text(root, wrap="word", width=40, height=15, font=(font_family, font_size))
 text.pack()
-
-pull_button = tk.Button(root, text="Pull", command=pull_document)
-pull_button.pack()
-
-save_button = tk.Button(root, text="Save", command=save_document)
-save_button.pack()
-
-download_button = tk.Button(root, text="Download", command=download_document)
-download_button.pack()
 
 client_socket = s
 
 root.mainloop()
+
